0fb6566a1ae0c2fb54bc5aa22d1899d653c0bb2e
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-02-16 02:28:37 -0300

    chore: add dotenv dependency

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`);
+});
 
 module.exports = app;
