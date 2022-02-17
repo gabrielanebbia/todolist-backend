2ad345e0ae480c49d9879db322cf3109765c0d22
Gabriela Nebbia <gabrielanebbia@gmail.com>
2022-02-17 06:22:40 -0300

    test: refactor task model tests adding a describe

--- src/tests/units/taskModel.test.js
+++ src/tests/units/taskModel.test.js
@@ -8,7 +8,7 @@ const taskModel = require('../../models/taskModel');
 
 const tasksMocks = require('../mocks/tasksMocks');
 
-describe('Get all tasks model', () => {
+describe('Task model', () => {
   let connectionMock;
 
   before(async () => {
@@ -22,67 +22,77 @@ describe('Get all tasks model', () => {
     MongoClient.connect.restore();
   });
 
-  describe('if has no tasks', () => {
-    it('should return an array', async () => {
-      const response = await taskModel.getAll();
+  describe('Get all tasks model', () => {
+    describe('if has no tasks', () => {
+      it('should return an array', async () => {
+        const response = await taskModel.getAll();
 
-      expect(response).to.be.a('array');
-    });
+        expect(response).to.be.a('array');
+      });
 
-    it('should return an empty array', async () => {
-      const response = await taskModel.getAll();
+      it('should return an empty array', async () => {
+        const response = await taskModel.getAll();
 
-      expect(response).to.be.empty;
-    });
-  });
-
-  describe('if has tasks', () => {
-    const tasks = [...tasksMocks.tasks];
-
-    before(async () => {
-      tasks.forEach((task) => {
-        connectionMock.db('modelTests').collection('task').insertOne(task);
+        expect(response).to.be.empty;
       });
     });
 
-    after(async () => {
-      await connectionMock.db('modelTests').collection('task').drop();
-    });
+    describe('if has tasks', () => {
+      const tasks = [...tasksMocks.tasks];
 
-    it('should return an array', async () => {
-      const response = await taskModel.getAll();
-
-      expect(response).to.be.a('array');
-    });
-
-    it('should return an array with tasks', async () => {
-      const response = await taskModel.getAll();
-
-      response.forEach((task, index) => {
-        expect(task).to.be.equal(tasks[index]);
+      before(async () => {
+        tasks.forEach((task) => {
+          connectionMock.db('modelTests').collection('task').insertOne(task);
+        });
       });
 
-      expect(response).to.have.length(tasks.length);
+      after(async () => {
+        await connectionMock.db('modelTests').collection('task').drop();
+      });
 
-      expect(response).to.include.all.keys('_id', 'task', 'status');
+      it('should return an array', async () => {
+        const response = await taskModel.getAll();
+
+        expect(response).to.be.a('array');
+      });
+
+      it('should return an array with tasks', async () => {
+        const response = await taskModel.getAll();
+
+        response.forEach((task, index) => {
+          expect(task).to.be.equal(tasks[index]);
+        });
+
+        expect(response).to.have.length(tasks.length);
+
+        expect(response).to.include.all.keys('_id', 'task', 'status');
+      });
     });
   });
-});
 
-describe('Create task model', () => {
-  describe('if a task is created', () => {
-    const task = tasksMocks.tasks[0];
+  describe('Create task model', () => {
+    describe('if a task is created', () => {
+      const task = tasksMocks.tasks[0];
 
-    it('should return an object', async () => {
-      const response = await taskModel.create(task);
+      it('should return an object', async () => {
+        const response = await taskModel.create(task);
 
-      expect(response).to.be.a('object');
-    });
+        expect(response).to.be.a('object');
+      });
 
-    it('should have a property "task', async () => {
-      const response = await taskModel.create(task);
+      it('should have a property "task', async () => {
+        const response = await taskModel.create(task);
 
-      expect(response).to.have.a.property('task');
+        expect(response).to.have.a.property('task');
+      });
+
+      it('should have a task created', async () => {
+        const taskCreated = await connectionMock
+          .db('modelTest')
+          .collection('tasks')
+          .findOne({ task });
+        expect(taskCreated).to.be.not.null;
+      });
     });
   });
 });
